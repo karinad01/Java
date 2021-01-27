9da6cdce039b99e8702d85bae7cff87093a8ec0e
Karina Doynova <kdoynova@gmail.com>
2021-01-27 19:57:52 +0100

    Java_Arrays

---
+++ JavaFundamentals/ARRAYS/LearningArrays/foreach_Demo.java
@@ -0,0 +1,11 @@
+package ArraysLecture;
+
+public class foreach_Demo {
+    public static void main(String[] args) {
+        String[] names = {"Some", "new", "Strings"};
+        for (String currentName : names) {
+            System.out.println(currentName);
+
+        }
+    }
+}
