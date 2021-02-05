cb1dd450f53a9c997a7328877b71e2756beccb66
Karina Doynova <kdoynova@gmail.com>
2021-02-05 13:44:26 +0100

    Java_Methods

---
+++ JavaFundamentals/METHODS/Calculations.java
@@ -0,0 +1,44 @@
+package Lab;
+
+import java.util.Scanner;
+
+public class Calculations {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String command = scan.nextLine();
+        int num1 = Integer.parseInt(scan.nextLine());
+        int num2 = Integer.parseInt(scan.nextLine());
+
+        switch (command) {
+            case "add":
+                add(num1, num2);
+                break;
+            case "multiply":
+                multiply(num1, num2);
+                break;
+            case "subtract":
+                subtract(num1, num2);
+                break;
+            case "divide":
+                divide(num1, num2);
+                break;
+        }
+
+    }
+
+    public static void add(int a, int b) {
+        System.out.println(a + b);
+    }
+
+    public static void multiply(int a, int b) {
+        System.out.println(a * b);
+    }
+
+    public static void subtract(int a, int b) {
+        System.out.println(a - b);
+    }
+
+    public static void divide(int a, int b) {
+        System.out.println(a / b);
+    }
+}
