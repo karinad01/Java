67ef8196ea03daa1a8a7b7837eb8b10d2c8fa4c8
Karina Doynova <kdoynova@gmail.com>
2021-02-12 20:05:31 +0100

    Create HashSetExample.java

---
+++ LearningJava/Collections/HashSet/HashSetExample.java
@@ -0,0 +1,19 @@
+package HashSet;
+
+import java.util.HashSet;
+import java.util.Iterator;
+
+public class HashSetExample {
+    public static void main(String[] args) {
+        HashSet<String> set = new HashSet();
+        set.add("One");
+        set.add("Two");
+        set.add("Three");
+        set.add("Four");
+        set.add("Five");
+        Iterator<String> i =set.iterator();
+        while(i.hasNext()){
+            System.out.println(i.next());
+        }
+    }
+}
