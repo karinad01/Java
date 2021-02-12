514de38119f075f5c5f6ad7d352a088d2f305952
Karina Doynova <kdoynova@gmail.com>
2021-02-12 19:08:38 +0100

    Create IteratingArraysForEach.java

---
+++ LearningJava/Collections/ArrayList/IteratingArraysForEach.java
@@ -0,0 +1,19 @@
+package ArrayList;
+
+import java.util.*;
+
+public class IteratingArraysForEach {
+    public static void main(String[] args) {
+
+
+        ArrayList<String> list = new ArrayList<String>();
+        list.add("Mango");
+        list.add("Apple");
+        list.add("Banana");
+        list.add("Grapes");
+
+        for (String fruit : list) {
+            System.out.println(fruit);
+        }
+    }
+}
