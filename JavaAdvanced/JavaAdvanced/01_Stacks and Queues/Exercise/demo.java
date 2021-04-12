67bad526a4ccbac702c471c8de87d14a23bc816b
Karina Doynova <kdoynova@gmail.com>
2021-04-12 21:33:30 +0300

    Create demo.java

---
+++ JavaAdvanced/JavaAdvanced/01_Stacks and Queues/Exercise/demo.java
@@ -0,0 +1,17 @@
+package Exercise;
+
+import java.util.ArrayDeque;
+import java.util.Scanner;
+
+public class demo {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        ArrayDeque<Integer> stack = new ArrayDeque<>();
+
+        stack.push(3);
+        stack.push(21);
+        stack.push(45);
+
+
+    }
+}
